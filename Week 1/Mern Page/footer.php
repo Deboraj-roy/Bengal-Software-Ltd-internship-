2704a5e5f7a3a6bf7156d99b9a57f272024bd357
DEBORAJ ROY <61079464+Deboraj-roy@users.noreply.github.com>
2022-09-01 17:28:50 +0600

    Add files via upload

---
+++ Week 1/Mern Page/footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: rgb(186, 18, 228);
+  color: white;
+}
+</style>
+</head>
+<body>
+ 
+</article>
+
+<footer>
+  <p>Author: © Deboraj<br>
+  <a href="mailto:deboraj@email.com">deboraj@email.com</a></p>
+</footer>
+
+
+
+
+
+<footer>
+
+    
+<div class="row">
+    <div class="column" >
+      <h2>  Product </h2>
+      <br>
+
+      <p>Listing</p>
+      <p>Property</p>
+      <p>Agents</p>
+      <p>Blog</p> 
+    </div>
+
+    <div class="column" >
+      <h2>Resources</h2>
+      <br>
+
+      <p>Our Homes</p>
+      <p>Member Stories</p>
+      <p>Video</p>
+      <p>Free trial</p> 
+    </div>
+
+    
+    <div class="column" >
+        <h2>Company</h2>
+        <br>
+  
+        <p>Patnerships</p>
+        <p>Terms of use</p>
+        <p>Privacy</p>
+        <p>Sitemap</p> 
+      </div>
+
+      
+    <div class="column" >
+        <h2>Get in touch</h2>
+        <br>
+  
+        <p>You’ll find your next home, in any style you prefer.</p> 
+      </div>
+
+      <br>
+      
+  </div>
+
+
+  <a class="navbar-brand" href="#">
+    <img src="Logo.png" alt="Logo" height="25">
+     </a>
+    <p>Author: © Deboraj<br>
+    <a href="mailto:deboraj@email.com">deboraj@email.com</a></p>
+</footer>
+
+<?php include('view/Include/Fooder.php') ?>
+
+
+</body>
+</html>
